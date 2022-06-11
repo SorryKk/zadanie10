b86a67b87e16cdd625038537ee8a867bc3314cba
SorryKk <106339489+SorryKk@users.noreply.github.com>
2022-06-11 12:44:42 +0500

    Add files via upload

---
+++ zadanie10/zadanie10.1.js
@@ -0,0 +1,181 @@
+//8.1.1.zadacha
+//let str="";
+//for(let i=1;i!=6;i++)
+//{
+//	str+="5 ";
+//}
+//for(let i=1;i!=6;i++)
+//{
+//	console.log(str);
+//}
+//8.1.2.zadacha
+//let str="";
+//for(let i=1;i!=11;i++){
+// str=str+i+" "
+//}
+//for(let i=1;i!=5;i++){
+// console.log(str)
+//}
+//8.1.3.zadacha
+//for(let i=4;i!=8;i++){
+// for(let j=1;j!=10;j++){
+// process.stdout.write(i+""+j+" ")
+//}
+// console.log()
+//}
+//8.2.1.zadacha
+//let str="";
+//for(let i=1;i!=6;i++){
+// str+="5 "
+// console.log(str)
+//}
+//8.2.2.zadacha
+//let str="";
+//for(let i=6;i!=1;i--){
+// for(let j=1;j!=i;j++){
+// process.stdout.write("1 ")
+//}
+// console.log()
+//}
+//8.3.1.zadacha
+//let str="";
+//for(let i=1;i!=6;i++){
+// for(let j=1;j<=i;j++){
+// process.stdout.write(i+" ")
+//}
+// console.log()
+//}
+//8.3.2.zadacha
+//let str="";
+//let num=5;
+//for(let i=6;i!=1;i--){
+// for(let j=1;j!=i;j++){
+// process.stdout.write(num+" ")
+//}
+// console.log()
+// num++
+//}
+//8.4.1.zadacha
+//let num=10;
+//let str="";
+//for(let i=1;i!=6;i++){
+// for(let j=1;j<=i;j++){
+// process.stdout.write(num+" ")
+//}
+// console.log()
+// num+=10
+//}
+//8.4.2.zadacha
+//let str="";
+//let num=5;
+//for(let i=6;i!=1;i--){
+// for(let j=1;j!=i;j++){
+// process.stdout.write(num+" ")
+//}
+// console.log()
+// num+=5
+//}
+//8.27.zadacha
+//function rabotaem(a)
+//{
+// let count=0;
+// for(let i=1;i<=9;i++)
+// {
+// if(a%i==0){count++}
+// }
+// return count;
+//}
+//
+//for(let i=1;i<=300;i++){
+// if(rabotaem(i)==5)(console.log(i))
+//}
+//8.28zadacha
+//function rabotaem(a)
+//{
+// let count=0;
+// for(let i=1;i<=9;i++)
+// {
+// if(a%i==0){count++}
+// }
+// return count;
+//}
+//
+//for(let i=200;i<=500;i++){
+// if(rabotaem(i)==6)(console.log(i))
+//}
+//8.29.zadacha
+//function rabotaem(a)
+//{
+// let count=0;
+// for(let i=1;i<=9;i++)
+// {
+// if(a%i==0){count++}
+// }
+// return count;
+//}
+//let a=1
+//let b=300
+//let k=5
+//for(let i=a;i<=b;i++){
+// if(rabotaem(i)==k)(console.log(i))
+//}
+//8.32zadacha
+//function rabotaem(a) {
+//    if(a < 2) return false;
+//    for (var i = 2; i < a; i++) {
+//        if(a%i==0)
+//            return false;
+//    }
+//    return true;
+//}
+//for(var i = 0; i < 100; i++){
+//    if(rabotaem(i)) console.log(i);
+//}
+//8.33zadacha
+//let count = 70;
+//let num = 0;
+//for (let i = 50; i <= count; i++)
+//for (let j = 50; j <= count; j++)
+//if (i % j == 0)
+//num += i;
+//console.log(num);
+//return 0;
+//8.34zadacha😎️
+//function rabotaem(a)
+//{
+// let count=0;
+// for(let i=1;i<=9;i++)
+// {
+// if(a%i==0){count+=i}
+// }
+// return count;
+//}
+//for(let i=100;i<=300;i++){
+//if(rabotaem(i)==50){console.log(i)}
+//}
+//8.35zadacha
+//function rabotaem(a)
+//{
+// let count=0;
+// for(let i=1;i<=9;i++)
+// {
+// if(a%i==0){count+=i}
+// }
+// return count;
+//}
+//for(let i=300;i<=600;i++){
+//if(rabotaem(i)%10==0){console.log(i)}
+//}
+//8.31zadacha
+//function rabotaem(a){
+//if(a==1||a==0)
+//{return false;}
+//else{
+//for(let i=2;i<a;i++){
+//if(a%i==0)
+//{return false;}
+//}
+//return true}
+//}
+//
+
